7b64e397e6df6dfee306617be514d6a99606989f
Allan Araujo <allanxp4@gmail.com>
2017-08-01 23:45:43 -0300

    commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <mem.h>
-
+#include <sys/time.h>
 #define INITIAL_POOL_SIZE 65536
 #define POOL_UPGRADE_EXTRA 65536
 #define INITIAL_ALLOCATION_SIZE 64
@@ -36,7 +36,10 @@ MPool global_pool;
 
 void* alok(int size) {
     if (size > global_pool.free) {
-        global_pool.start_pointer = realloc(global_pool.start_pointer, (size_t) (size + POOL_UPGRADE_EXTRA));
+        int reallocSize = size + POOL_UPGRADE_EXTRA;
+        global_pool.start_pointer = realloc(global_pool.start_pointer, reallocSize);
+        global_pool.free += reallocSize;
+        global_pool.pool_size += reallocSize;
     }
     Allocation a = firstFit(size);
     return a.pointer;
@@ -56,7 +59,7 @@ Allocation firstFit(int size) {
 Allocation getNewAllocation(int size){
     Allocation allocation;
     if(global_pool.is_initialized){
-        allocation.pointer = global_pool.allocations[global_pool.num_allocations - 1].pointer + global_pool.allocations[global_pool.num_allocations - 1].size;
+        allocation.pointer = global_pool.allocations[global_pool.num_allocations - 1].pointer + (global_pool.allocations[global_pool.num_allocations - 1].size / 4);
     }
     else{
         allocation.pointer = global_pool.start_pointer;
@@ -83,6 +86,7 @@ void delok(void* pointer) {
     }
 }
 
+
 void initAllocator() {
     void* basePointer = malloc(INITIAL_POOL_SIZE);
     global_pool.start_pointer = basePointer;
@@ -90,17 +94,35 @@ void initAllocator() {
     global_pool.free = INITIAL_POOL_SIZE;
 }
 
-void main(){
-    initAllocator();
-    char* batata = (char *) alok(sizeof(char) + 100);
-    char* batata2 = (char *) alok(sizeof(char) + 20);
-    char* batata3 = (char *) alok(sizeof(char) + 38);
-    strcpy(batata, "oi");
-    strcpy(batata2, "entao");
-    strcpy(batata3, "mano");
-    printf("%s", batata);
-    printf("%s", batata2);
-    printf("%s", batata3);
-    getchar();
+int* batata;
 
+long getMicrotime();
+
+long getMicrotime() {
+    return clock();
+}
+
+int main(){
+    initAllocator();
+    long startTime = getMicrotime();
+    for(int i = 0; i < 1000; i++){
+        batata = malloc(sizeof(int));
+    }
+    printf("malloc: %ld\n", getMicrotime() - startTime);
+    startTime = getMicrotime();
+    for(int i = 0; i < 1000; i++){
+        batata = alok(sizeof(int));
+    }
+    printf("alok: %ld\n", getMicrotime() - startTime);
+//    char* batata = (char *) alok(sizeof(char) + 70000);
+//    char* batata2 = (char *) alok(sizeof(char) + 150000);
+//    char* batata3 = (char *) alok(sizeof(char) + 38);
+//    strcpy(batata, "oi");
+//    strcpy(batata2, "entao");
+//    strcpy(batata3, "mano");
+//    printf("%s", batata);
+//    printf("%s", batata2);
+//    printf("%s", batata3);
+    getchar();
+    return 0;
 }
